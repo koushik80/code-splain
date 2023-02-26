525155b5ed60edd765efb3ef852279cfb646c9db
Koushik Dey <koushik.dey@live.com>
2023-02-27 00:58:59 +0200

    test for checking icon, link

--- src/components/repositories/RepositoriesListItem.test.js
+++ src/components/repositories/RepositoriesListItem.test.js
@@ -10,7 +10,9 @@ function renderComponent() {
       full_name: 'facebook/react',
       language: 'Javascript',
       description: 'A JS library',
-      owner: 'facebook',
+      owner: {
+        login: 'facebook'
+      },
       name: 'react',
       html_url: 'https://github.com/facebook/react'
     };
@@ -33,6 +35,30 @@ test('shows a link to the github homepage for this repository', async() => {
   })
   expect(link).toHaveAttribute('href', repository.html_url);
 });
+
+test('shows a file icon with the appropriate icon', async () => {
+  renderComponent();
+
+  const icon = await screen.findByRole('img', { name: 'Javascript' });
+
+  expect(icon).toHaveClass('js-icon');
+});
+
+test('shows a link to the code editor page', async() => {
+  const { repository } = renderComponent();
+
+  await screen.findByRole('img', { name: 'Javascript' });
+
+  const link = await screen.findByRole('link', {
+    name: new RegExp(repository.owner.login)
+  });
+
+  expect(link).toHaveAttribute('href', `/repositories/${repository.full_name}`)
+});
+
+
+
+
 
 /*
 const pause = () => {
