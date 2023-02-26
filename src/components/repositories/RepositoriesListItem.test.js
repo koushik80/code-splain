5b0ed00dbc7b0fbaaf91d4bf9371d5f36714318a
Koushik Dey <koushik.dey@live.com>
2023-02-27 00:04:19 +0200

    refactored RepositoriesListItem component and added test for link

--- src/components/repositories/RepositoriesListItem.test.js
+++ src/components/repositories/RepositoriesListItem.test.js
@@ -12,19 +12,26 @@ function renderComponent() {
       description: 'A JS library',
       owner: 'facebook',
       name: 'react',
-      html_ur: 'https://github.com/facebook/react'
+      html_url: 'https://github.com/facebook/react'
     };
   render(
     <MemoryRouter>
       <RepositoriesListItem repository={repository} />
     </MemoryRouter>
   );
+
+  return { repository };
   };
 
 test('shows a link to the github homepage for this repository', async() => {
-  renderComponent();
+  const { repository } = renderComponent();
 
-  await screen.findByRole('img', { name: 'Javascript'});
+  await screen.findByRole('img', { name: 'Javascript' });
+
+  const link = screen.getByRole('link', {
+    name: /github repository/i
+  })
+  expect(link).toHaveAttribute('href', repository.html_url);
 });
 
 /*
